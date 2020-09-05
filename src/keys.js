3d6dd6d4a4680159c39b8a72b2fec2e9349150a9
owen araujo <owendjesus64@gmail.com>
2020-09-04 20:43:18 -0400

    hacer interfaz de menu responsy

--- src/keys.js
+++ src/keys.js
@@ -1,20 +1,20 @@
-module.exports = {
+// module.exports = {
 
-    database: {
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'sistema_mantenimiento',
-        multipleStatements: true
-    }
-
-};
-// module.exports={
-//     database:{
-//         host:'bz0xjhxhz6tjnzaexpex-mysql.services.clever-cloud.com',
-//         user: 'upgpxzlo6c477mrh',
-//         password:'QWHXzCR78uZejRjqZAo3',
-//         database:'bz0xjhxhz6tjnzaexpex'
+//     database: {
+//         connectionLimit: 10,
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'sistema_mantenimiento',
+//         multipleStatements: true
 //     }
-// }
+
+// };
+module.exports = {
+  database: {
+    host: "bz0xjhxhz6tjnzaexpex-mysql.services.clever-cloud.com",
+    user: "upgpxzlo6c477mrh",
+    password: "QWHXzCR78uZejRjqZAo3",
+    database: "bz0xjhxhz6tjnzaexpex",
+  },
+};
